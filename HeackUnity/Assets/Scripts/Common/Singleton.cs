b62ca188415d194750af87330d7984811b4c04af
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-09-12 11:25:34 -0400

    added class singleton

---
+++ HeackUnity/Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+
+	public static T Instance { get; private set; }
+
+	protected virtual void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this as T;
+		}
+		else
+		{
+			Debug.LogError("There can only be one instance of " + typeof(T).Name);
+
+			gameObject.SetActive(false);
+		}
+	}
+}
