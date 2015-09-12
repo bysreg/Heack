0fc5798a4e3bff1ce97642be62f0287653a60268
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-09-12 07:28:37 -0400

    added skeleton code

---
+++ HeackUnity/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using BladeCast;
+
+namespace Heack
+{
+    public class GameController : MonoBehaviour
+    {
+        void Start()
+        {
+            InitControllerListeners();
+        }
+
+        void InitControllerListeners()
+        {
+            BCMessenger.Instance.RegisterListener("connect", 0, this.gameObject, "HandleControllerConnected");
+        }
+
+        void HandleControllerConnected()
+        {
+            print("Connected to controller");
+        }
+    }
+
+}
