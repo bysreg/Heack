3f8920ef57cd6c8b5d8adcbbe1add1c94e1a302b
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-09-13 00:51:31 -0400

    added Timer class and gameobject

---
+++ HeackUnity/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Haeck
+{
+    public class Timer : MonoBehaviour
+    {
+
+        float timeLeft;
+        [SerializeField]
+        float maxTime;
+
+        [SerializeField]
+        Text timerText;
+
+        void Awake()
+        {
+            timeLeft = maxTime;
+        }
+
+        void Update()
+        {
+            timeLeft -= Time.deltaTime;
+
+            timerText.text = (((int)(timeLeft * 100f)) / 100f) + "";
+        }
+
+    }
+
+}
